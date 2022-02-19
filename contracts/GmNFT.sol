0d4fe514f67733e0174757dddc4bb9035677bff8
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2022-02-19 16:29:21 +0000

    set svg data

--- contracts/GmNFT.sol
+++ contracts/GmNFT.sol
@@ -13,7 +13,7 @@ contract GmNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("GM", "GM") {
+    constructor() ERC721("GM.", "GM..") {
         console.log("Hello from my not-so-smart contract");
     }
 
@@ -26,7 +26,7 @@ contract GmNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/ZW0D");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiJnbSIsCiAgICAiZGVzY3JpcHRpb24iOiJnZCIsCiAgICAiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStaMjA4TDNSbGVIUStDand2YzNablBnPT0iCn0");
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
