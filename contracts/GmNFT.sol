48b3e2abb3e3ee44cef686b672cf1a1fa6e47734
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2022-02-19 15:17:29 +0000

    Add make nft method to contract

--- contracts/GmNFT.sol
+++ contracts/GmNFT.sol
@@ -2,10 +2,35 @@
 
 pragma solidity ^0.8.0;
 
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract GmNFT {
-    constructor() {
+contract GmNFT is ERC721URIStorage {
+
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("GM", "GM") {
         console.log("Hello from my not-so-smart contract");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, '{"name":"I dont always work on my side projects","description":"but when i do i dont finish them","image":"https://i.imgur.com/bRZEywl.png"}');
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
     }
 }
